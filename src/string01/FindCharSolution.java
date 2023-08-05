ff1ffc461fbdcc34bd3426753cb9e1c29d075705
imgonnabe <qorlwn456123@gmail.com>
2023-08-05 15:48:38 +0900

    문자 찾기

---
+++ src/string01/FindCharSolution.java
@@ -0,0 +1,26 @@
+package string01;
+
+import java.util.Scanner;
+
+public class FindCharSolution {
+	public static int solution(String s, char c) {
+		int answer = 0;
+		s = s.toUpperCase();
+		c = Character.toUpperCase(c);
+		
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) == c) {
+				++answer;
+			}
+		}
+		return answer;
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String input1 = sc.next();
+		char input2 = sc.next().charAt(0);
+		
+		System.out.println(FindCharSolution.solution(input1, input2));
+	}
+}
