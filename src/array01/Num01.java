c5ae2c70830092bb642ec786f76133fe7763f7a0
imgonnabe <qorlwn456123@gmail.com>
2023-08-07 23:30:07 +0900

    코테

---
+++ src/array01/Num01.java
@@ -0,0 +1,26 @@
+package array01;
+
+import java.util.Scanner;
+
+public class Num01 {
+	public void solution(int N, int[] arr) {
+		for (int i = 1; i < arr.length; i++) {
+			if (arr[i] > arr[i - 1]) {
+				System.out.print(arr[i] + " ");
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] input1 = new int[N];
+		for (int i = 0; i < input1.length; i++) {
+			input1[i] = sc.nextInt();
+		}
+		
+		Num01 n = new Num01();
+		System.out.print(input1[0] + " ");
+		n.solution(N, input1);
+	}
+}
---
+++ src/array01/Student01.java
@@ -0,0 +1,29 @@
+package array01;
+
+import java.util.Scanner;
+
+public class Student01 {
+	public int solution(int n, int[] arr) {
+		int max = arr[0];
+		int answer = 1;
+		for (int i = 1; i < arr.length; i++) {
+			if (arr[i] > max) {
+				answer++;
+				max = arr[i];
+			}
+		}
+		return answer;
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] arr = new int[N];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		Student01 stud = new Student01();
+		System.out.println(stud.solution(N, arr));
+	}
+}
