3e21e119ed961a3b7d972f7d20b293efca724928
imgonnabe <qorlwn456123@gmail.com>
2023-08-13 15:14:15 +0900

    점수계산

---
+++ src/calculate/Cal.java
@@ -0,0 +1,29 @@
+package calculate;
+
+import java.util.Scanner;
+
+public class Cal {
+	public static int solution(int N, int[] arr) {
+		int cnt = 0;
+		int answer = 0;
+		for (int i = N - 1; i >= 0; i--) {
+			if(arr[i] == 1) {
+				cnt += 1;
+				answer += cnt;
+			} else if (arr[i] == 0) {
+				cnt = 0;
+			}
+		}
+		return answer;
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] arr = new int[N];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = sc.nextInt();
+		}
+		System.out.println(solution(N, arr));
+	}
+}
