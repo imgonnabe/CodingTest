44272d6ae56e797dc5bac46b3c0794897484454a
imgonnabe <qorlwn456123@gmail.com>
2023-08-18 06:52:14 +0900

    멘토링

--- src/mentoring/Mentoring.java
+++ src/mentoring/Mentoring.java
@@ -3,9 +3,30 @@ package mentoring;
 import java.util.Scanner;
 
 public class Mentoring {
-	public static int solutiion(int n, int m, int[][] arr) {
+	public static int solution(int n, int m, int[][] arr) {
 		int answer = 0;
-		
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n; j++) {
+				int cnt = 0;
+				for (int k = 0; k < m; k++) {
+					int pi = 0, pj = 0;
+					for (int s = 0; s < n; s++) {
+						if(arr[k][s] == i) {
+							pi = s;
+						}
+						if(arr[k][s] == j) {
+							pj = s;
+						}
+					}
+					if(pi < pj) {
+						cnt++;
+					}
+				}
+				if(cnt == m) {
+					answer++;
+				}
+			}
+		}
 		return answer;
 	}
 	
@@ -19,5 +40,6 @@ public class Mentoring {
 				arr[i][j] = sc.nextInt();
 			}
 		}
+		System.out.println(solution(n, m ,arr));
 	}
 }
