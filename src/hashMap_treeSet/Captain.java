bfa1b41a5b07ad91a058830f0ec235ce543c6bbf
imgonnabe <qorlwn456123@gmail.com>
2023-08-26 22:49:30 +0900

    학급회장

---
+++ src/hashMap_treeSet/Captain.java
@@ -0,0 +1,30 @@
+package hashMap_treeSet;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Captain {
+	public static char solution(int n, String str) {
+		char answer = ' ';
+		HashMap<Character, Integer> map = new HashMap<>();
+		for (char x : str.toCharArray()) {
+			map.put(x, map.getOrDefault(x, 0) + 1);
+		}
+		
+		int max = Integer.MIN_VALUE;
+		for (char key : map.keySet()) {
+			if(map.get(key) > max) {
+				max = map.get(key);
+				answer = key;
+			}
+		}
+		return answer;
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		String str = sc.next();
+		System.out.println(solution(n, str));
+	}
+}
