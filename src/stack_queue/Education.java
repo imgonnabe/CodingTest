32957f6d2862bf5e3587de8f5963fb202328e833
imgonnabe <qorlwn456123@gmail.com>
2023-08-30 07:00:14 +0900

    교육과정설계(Queue)

---
+++ src/stack_queue/Education.java
@@ -0,0 +1,34 @@
+package stack_queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Education {
+	public static String solution(String str, String str2) {
+		String answer = "YES";
+		Queue<Character> Q = new LinkedList<Character>();
+		for (char c : str.toCharArray()) {
+			Q.offer(c);
+		}
+		
+		for (char c : str2.toCharArray()) {
+			if(Q.contains(c)){
+				if(c != Q.poll()) {
+					return "NO";
+				}
+			}
+		}
+		if(!Q.isEmpty()) {
+			return "NO";
+		}
+		return answer;
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		String str2 = sc.next();
+		System.out.println(solution(str, str2));
+	}
+}
