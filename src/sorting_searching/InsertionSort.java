f136afdb8c7cd57b6b5fc16b45e9cb2d0b834300
imgonnabe <qorlwn456123@gmail.com>
2023-09-01 23:45:42 +0900

    삽입정렬

---
+++ src/sorting_searching/InsertionSort.java
@@ -0,0 +1,32 @@
+package sorting_searching;
+
+import java.util.Scanner;
+
+public class InsertionSort {
+	public static int[] solution(int n, int[] a) {
+		for (int i = 0; i < n; i++) {
+			int tmp = a[i], j;
+			for (j = i - 1; j >= 0; j--) {
+				if(a[j] > tmp) {
+					a[j + 1] = a[j];
+				} else {
+					break;
+				}
+			}
+			a[j + 1] = tmp;
+		}
+		return a;
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] a = new int[n];
+		for (int i = 0; i < n; i++) {
+			a[i] = sc.nextInt();
+		}
+		for (int i : solution(n,a)) {
+			System.out.print(i + " ");
+		}
+	}
+}
