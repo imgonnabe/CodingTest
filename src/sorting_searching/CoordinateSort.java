3b364722ec2afafcd22845a098ff3647d04cd616
imgonnabe <qorlwn456123@gmail.com>
2023-09-04 23:35:31 +0900

    좌표정렬(compareTo)

--- src/sorting_searching/CoordinateSort.java
+++ src/sorting_searching/CoordinateSort.java
@@ -1,27 +1,39 @@
 package sorting_searching;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
-public class CoordinateSort {
-	public static ArrayList<Integer> solution(int n, int[][] a) {
-		ArrayList<Integer> answer = new ArrayList<>();
-		Arrays.sort(a);
-		return answer;
+class Point implements Comparable<Point> {
+	public int x, y;
+	public Point(int x, int y) {
+		this.x = x;
+		this.y = y;
 	}
 	
+	@Override
+	public int compareTo(Point obj) {
+		if(this.x == obj.x) {
+			return this.y - obj.y;// 오름차순(1 -> 5) : 1-5(음수)가 되어야 한다.
+		} else {// 내림차순(5 -> 1) : 1-5(음수)가 되어야 한다. obj.y - this.y
+			return this.x - obj.x;
+		}
+	}
+}
+
+public class CoordinateSort {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		int[][] a = new int[n][2];
+		ArrayList<Point> arr = new ArrayList<>();
 		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < 2; j++) {
-				a[i][j] = sc.nextInt();
-			}
+			int x = sc.nextInt();
+			int y = sc.nextInt();
+			arr.add(new Point(x, y));
 		}
-		for (int is : solution(n, a)) {
-			System.out.print(is + " ");
+		Collections.sort(arr);
+		for (Point point : arr) {
+			System.out.println(point.x + " " + point.y);
 		}
 	}
 }
