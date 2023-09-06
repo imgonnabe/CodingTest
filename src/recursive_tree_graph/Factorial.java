a8f602f9e04393d89c5285da71ba28fcb29ba5e1
imgonnabe <qorlwn456123@gmail.com>
2023-09-07 06:45:47 +0900

    팩토리얼(재귀함수)

---
+++ src/recursive_tree_graph/Factorial.java
@@ -0,0 +1,15 @@
+package recursive_tree_graph;
+
+public class Factorial {
+	public static int DFS(int n) {
+		if(n == 1) {
+			return 1;
+		} else {
+			return n * DFS(n - 1);
+		}
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(DFS(5));
+	}
+}
